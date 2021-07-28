05e309d797e065bde8503438b3988eaf9be853ab
Magnus Fredslund <fredslundmagnus@gmail.com>
2021-07-28 20:26:52 +0200

    Fix

--- lib/printer.dart
+++ lib/printer.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import '_printer.dart';
 
 String colored(Object? object, Color color) {
-  return "\x1B[38;2;${color.red};${color.green};${color.blue}m$object\x1B[0m";
+  return '\x1B[38;2;${color.red};${color.green};${color.blue}m$object\x1B[0m';
 }
 
 void print(Object? object, [MaterialColor? color]) {
@@ -10,7 +10,5 @@ void print(Object? object, [MaterialColor? color]) {
 }
 
 void building(Object? object) {
-  print("Is building ${_colored(object, Colors.green)}!");
+  print('Is building ${colored(object, Colors.green)}!');
 }
-
-
