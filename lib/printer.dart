14ac291351887d46c0577244bb84dc8ef1b31ad8
Magnus Fredslund <fredslundmagnus@gmail.com>
2021-07-29 13:26:15 +0200

    Fix

--- lib/printer.dart
+++ lib/printer.dart
@@ -5,7 +5,7 @@ String colored(Object? object, Color color) {
   return '\x1B[38;2;${color.red};${color.green};${color.blue}m$object\x1B[0m';
 }
 
-void print(Object? object, [MaterialColor? color]) {
+void print(Object? object, [Color? color]) {
   color == null ? printNormal(object) : printNormal(colored(object, color));
 }
 
